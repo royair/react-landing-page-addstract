1aa96bf32c90e7c3c094aaee10471d19ef13ea01
roy yair <roy.yair@gmail.com>
2019-01-10 06:25:18 +0200

    Updates

---
+++ precache-manifest.d24643454bf8cb417305c2a84d343435.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cb99d561b218b19d049",
+    "url": "/react-landing-page-addstract/static/js/runtime~main.7cb99d56.js"
+  },
+  {
+    "revision": "84c006ba353364cd9f46",
+    "url": "/react-landing-page-addstract/static/js/main.84c006ba.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/react-landing-page-addstract/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "84c006ba353364cd9f46",
+    "url": "/react-landing-page-addstract/static/css/main.ccec55dd.chunk.css"
+  },
+  {
+    "revision": "57f64346e6f9af5ffb210130a0f9aea4",
+    "url": "/react-landing-page-addstract/index.html"
+  }
+];
